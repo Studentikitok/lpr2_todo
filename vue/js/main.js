3ccb916b45fb16d5c0ae20b48128655444ab3efb
StraponBace <ponomarev.tima2005@mail.ru>
2024-02-15 10:52:41 +0700

    Create layout

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,5 +1,13 @@
 Vue.component('ToDoApp'){
     template:`
-        
+        <header>
+            <h1 class="app-title">To Do</h1>
+            <p class="app-description">Just do it</p>
+        </header>
+        <main>
+            <TaskOnView></TaskOnView>
+            <TaskOnWork></TaskOnWork>
+            <TaskDone></TaskDone>
+        </main>
     `
 }
