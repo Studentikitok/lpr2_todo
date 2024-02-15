0fd457653e0a8006dacb3e28810059b081d40334
StraponBace <ponomarev.tima2005@mail.ru>
2024-02-15 11:55:01 +0700

    Create layout base on html and set margin&padding

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,13 +1,29 @@
-Vue.component('ToDoApp'){
+Vue.component('ToDoApp',{
     template:`
         <header>
             <h1 class="app-title">To Do</h1>
             <p class="app-description">Just do it</p>
+            <TaskCreate></TaskCreate>
         </header>
         <main>
-            <TaskOnView></TaskOnView>
+            
+            <TaskOnStart></TaskOnStart>
             <TaskOnWork></TaskOnWork>
             <TaskDone></TaskDone>
         </main>
     `
-}
+})
+
+Vue.component('TaskCreate', {
+    template:`
+        <div class="create-task">
+            <input type="text" name="" id="">
+        <\div>
+    `
+})
+
+Vue.component('TaskOnStart', {
+    template:`
+        
+    `
+})
