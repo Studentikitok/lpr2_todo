81b06b67b5f6863f83d393cc98a2458c6cb85655
StraponBace <ponomarev.tima2005@mail.ru>
2024-02-20 14:02:53 +0700

    Finally I understood what the problem was with displaying the component, and also added the logic for calling task list items

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,28 +1,28 @@
 Vue.component('to-do-app',{
     template:`
-        <header>
-            <h1 class="app-title">To Do</h1>
-            <p class="app-description">Just do it</p>
-            <task-create></task-create>
-        </header>
-        <main>
-            <div class="tasks">
-                <div class="task-on-start">
+        <div>
+            <header>
+                <h1 class="app-title">To Do</h1>
+                <p class="app-description">Just do it</p>
+                <task-create></task-create>
+            </header>
+            <main>
+                <div class="tasks">
                     <task-on-start></task-on-start>
                 </div>
-            </div>
-            <div class="tasks">
-                <div class="task-on-work">
-                    <task-on-work></task-on-work>
-                </div>
-            </div>
-            <div class="tasks">
-                <div class="task-done">
-                    <task-on-done></task-on-done>
-                </div>
-            </div>
-            
-        </main>
+            </main>
+    <!--            <div class="tasks">-->
+    <!--                <div class="task-on-work">-->
+    <!--                    <task-on-work></task-on-work>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!--            <div class="tasks">-->
+    <!--                <div class="task-done">-->
+    <!--                    <task-on-done></task-on-done>-->
+    <!--                </div>-->
+    <!--            </div>-->
+        </div>  
+        
     `
 })
 
@@ -41,21 +41,29 @@ Vue.component('task-create', {
 })
 
 Vue.component('task-on-start', {
-    template:`
-        <div class="tasks">
+    template: `
             <div class="task">
-                <h3> dddddddddk</h3>
+                <h3>title</h3>
                 <ul>
-                    <li><input type="checkbox" name="" id=""> <p>  ddddddddddddddddddddd ddddddddtem</p></li>
-                    <li><input type="checkbox" name="" id=""> <p>item</p></li>
-                    <li><input type="checkbox" name="" id=""> <p>item</p></li>
-                    <li><input type="checkbox" name="" id=""> <p>item</p></li>
-                    <li><input type="checkbox" name="" id=""> <p>item</p></li>
+                    <li v-for="item in items" :key="item.id">
+                        <input type="checkbox" :id="item.id"> <p>{{ item.text }}</p>
+                    </li>
                 </ul>
             </div>
-        </div>
-    `
+       `,
+    data() {
+        return {
+            items: [
+                { id: 1, text: 'Item one' },
+                { id: 2, text: 'Item two' },
+                { id: 3, text: 'Item three' },
+                { id: 4, text: 'Item four' },
+                { id: 5, text: 'Item five' }
+            ]
+        }
+    }
 })
+
 
 let app = new Vue({
     el: '#app',
